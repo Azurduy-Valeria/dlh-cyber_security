5e881c316af790f150ef9e37cd7919a3a58815d8
Azurduy-Valeria <valeria.guzman@alumni.uni.lu>
2026-05-27 23:00:12 +0200

    updated script

--- linux_security/network_protocols/2-harden.sh
+++ linux_security/network_protocols/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -o+w 2>/dev/null | while read d; do echo "$d"; chmod o-w "$d"; done
+find / -type d -perm -o+w 2>/dev/null | while read d; do printf '%s\n' "$d"; chmod o-w "$d"; done
