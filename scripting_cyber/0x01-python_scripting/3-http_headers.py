fd6c24563ab76d56deab32b68122d92829835683
Azurduy-Valeria <valeria.guzman@alumni.uni.lu>
2026-06-11 09:28:11 +0200

    added script

---
+++ scripting_cyber/0x01-python_scripting/3-http_headers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import requests
+
+
+def get_http_headers(url):
+        response = requests.get(url)
+        return {
+            'status_code': response.status_code,
+            'headers': dict(response.headers)
+        }
+    except requests.exceptions.RequestException:
+        return None
+
+
+if __name__ == "__main__":
+    print(get_http_headers("https://www.google.com"))
